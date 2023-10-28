388f663b9e5572c19f2d69fe4926dab3a798f93e
enzo <enzocarnevali708@gmail.com>
2023-10-28 01:01:45 -0300

    create initial resources

---
+++ main.tf
@@ -0,0 +1,37 @@
+################################################################################
+# Resource Group
+################################################################################
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "minecraftRG"
+  location = "Brazil South"
+}
+
+
+################################################################################
+# Container App
+################################################################################
+
+resource "azurerm_container_group" "mineContainer" {
+  name                = "mineContainer"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  location            = azurerm_resource_group.resourcegroup.location
+  os_type             = "Linux"
+  ip_address_type     = "Public"
+  container {
+    name         = "minecraft"
+    image        = "itzg/minecraft-server"
+    cpu          = 1
+    memory       = 1
+    cpu_limit    = 2
+    memory_limit = 2
+    ports {
+      port     = 25565
+      protocol = "tcp"
+    }
+    # volume {
+    #   name = "mineData"
+    #   mount_path = "/mineData"
+    # }
+  }
+}
